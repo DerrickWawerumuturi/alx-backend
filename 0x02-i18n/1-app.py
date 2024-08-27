cf461245dfbba0fd03ceb3fb364c2e33666eae22
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-08-28 02:33:48 +0300

    flask-babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" import flask, babel and render template
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+""" istantiate babel
+"""
+babel = Babel()
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel.init_app(app)
+
+
+@app.route('/')
+def index():
+    """ render home page
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" import flask, babel and render template
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+""" istantiate babel
+"""
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_local():
+    """ retrieves the locale for a web page
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """ render home page
+    """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
