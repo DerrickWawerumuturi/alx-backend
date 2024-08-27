3039c4c5c0f460be55c12ba23a64e33359cd7ed9
DerrickWawerumuturi <wawerumuturi57@gmail.com>
2024-08-28 02:46:47 +0300

    pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,10 +8,13 @@ from flask_babel import Babel
 """ istantiate babel
 """
 babel = Babel()
+
+
 class Config:
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
